db914e97695b697fa0dc9bab27d6a79768dd1b6b
Cory Henderson <chenderson19899@gmail.com>
2019-01-09 16:59:48 -0800

    added questions 6 and 7 for lab 3. also added 'use strict' to js file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+'use strict';
 alert('We\'re going to play a guessing game!');
 alert('Guess answers to the following questions to learn a little more about me!');
 
 var counter = 0;
 
+//QUESTION 1
 var birthPlace = prompt('I was born in Seattle, WA (Y/N)');
 if(birthPlace.toUpperCase() === 'N' || birthPlace.toUpperCase() === 'NO'){
     alert('Correct! I was born in Seoul, South Korea.');
@@ -12,6 +14,7 @@ if(birthPlace.toUpperCase() === 'N' || birthPlace.toUpperCase() === 'NO'){
 }
 console.log('Answer1: ', birthPlace);
 
+//QUESTION 2
 var roomMates = prompt('I live with 15 animals (Y/N)');
 if(roomMates.toUpperCase() === 'Y' || roomMates.toUpperCase() === 'YES'){
     alert('Correct! 1 dog, 8 chickens, and 6 humans!');
@@ -21,6 +24,7 @@ if(roomMates.toUpperCase() === 'Y' || roomMates.toUpperCase() === 'YES'){
 }
 console.log('Answer2: ', roomMates);
 
+//QUESTION 3
 var food = prompt('Pineapple makes everything better. (Y/N)');
 if(food.toUpperCase() === 'Y' || food.toUpperCase() === 'YES'){
     alert('Agreed, how else would you make the perfect pizza, burger, or tacos?');
@@ -30,6 +34,7 @@ if(food.toUpperCase() === 'Y' || food.toUpperCase() === 'YES'){
 }
 console.log('Answer3: ', food);
 
+//QUESTION 4
 var invention = prompt('Computers are human\'s greatest invention. (Y/N)');
 if(invention.toUpperCase() === 'N' || invention.toUpperCase() === 'NO'){
     alert('Personally, I think the bicycle is the coolest. Mountain bikes, road bikes, motor bikes...perfectly designed fun');
@@ -39,6 +44,7 @@ if(invention.toUpperCase() === 'N' || invention.toUpperCase() === 'NO'){
 }
 console.log('Answer4: ', invention);
 
+//QUESTION 5
 var music = prompt('My favorite type of music is rap. (Y/N)');
 if(music.toUpperCase() === 'N' || music.toUpperCase() === 'NO'){
     alert('Correct. I like good old rock and roll.');
@@ -48,3 +54,58 @@ if(music.toUpperCase() === 'N' || music.toUpperCase() === 'NO'){
 }
 console.log('Answer5: ', music);
 console.log('You answered ' + counter + ' out of 5 questions correctly!');
+
+
+//QUESTION 6 - Guess a number with only 4 chances
+var guessCounter = 0;
+var answer = 12;
+
+for(var i=0; i<4; i++){
+    guessCounter++;
+    var remainingGuesses = 4-guessCounter;
+    var guessNumber = prompt('Guess how many years I\'ve lived in Seattle');
+
+    if(parseInt(guessNumber) === answer && guessCounter === 1){
+        alert('Congrats! You answered correctly on your first try!');
+        break;
+    }else if(parseInt(guessNumber) === answer){
+        alert('Correct! You guessed correctly in ' + guessCounter + ' guesses!');
+        break;
+    }else if(parseInt(guessNumber) > answer){
+        alert('Your guess was too high. You have ' + remainingGuesses + ' guesses left.');
+    }else if(parseInt(guessNumber) < answer && remainingGuesses === 0){ 
+        alert('Your guess was too low.  You have no remaining guesses. The correct answer was ' + answer +'.');
+    }else{
+        alert('Your guess was too low. You have ' + remainingGuesses + ' guesses left.');
+    }
+}
+
+//QUESTION 7 - Guess a correct answer from an array of correct answers within 6 tries
+var placesLived = ['tuscon', 'paris', 'madrid', 'seoul', 'richland'];
+var guessCounter = 0;
+var continueGuessing = true;
+var incorrectGuess = false;
+
+for(var i=0; i<6; i++){
+    guessCounter++;
+    var remainingGuesses = 6-guessCounter;
+
+    if(remainingGuesses > 0 && continueGuessing === true){
+        if(incorrectGuess === true){
+            alert('Nope, I haven\'t lived there. You have ' + remainingGuesses + ' guesses left.');
+        }
+
+        var guessCity = prompt('Guess one city I\'ve lived in other than Seattle');
+        for(var j=0; j < placesLived.length; j++){
+            if(placesLived[j] === guessCity.toLowerCase()){
+                alert('Correct! You guessed correctly in ' + guessCounter + ' guesses!');
+                var continueGuessing = false;
+            }
+        }
+        incorrectGuess = true;
+    }
+
+    if(remainingGuesses === 0){
+        alert('You\'re out of guesses! Correct answers were: ' + placesLived);
+    }
+}     
